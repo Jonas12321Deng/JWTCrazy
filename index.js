73f2912878464fdeb96cd9dfe8d93b32eee66403
Jonas Deng <zhiqin.deng@dfiretailgroup.com>
2025-05-06 14:53:23 +0800

    update jwt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
   res.status(200).json({
-    jwt: "你的最新 JWT 字符串"
+    jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21QYXJhbWV0ZXIiOiJxd2VyMTIzNCIsImV4cCI6MTc0NzExODIyOSwidXNlcklkIjoxNzM0NjkwfQ.VpspHGdx9z3Ftw573ols6ERa0GMOsUC-gcl0ctxkKes"
   });
 };
