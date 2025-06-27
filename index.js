1973dd1888fc0738478fede0779c0e92c3453df5
Jonas Deng <zhiqin.deng@dfiretailgroup.com>
2025-06-27 14:14:13 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
   res.status(200).json({
-    jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21QYXJhbWV0ZXIiOiJxd2VyMTIzNCIsImV4cCI6MTc1MDkxODI4NCwidXNlcklkIjoxNzM0NjkwfQ.zuZYSj0pCrKAR4Yngtwcl_DLdghmhwK0Hz0BjbYyrwY"
+    jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21QYXJhbWV0ZXIiOiJxd2VyMTIzNCIsImV4cCI6MTc1MTYwOTU5NSwidXNlcklkIjoxNzM0NjkwfQ.RS2wMPIrG_2oWZVNxEbuvpYRIjB_8ePB-hlrEov1w1o"
   });
 };
