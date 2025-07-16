6bc22290d2af36c614fc775c56a5b15832695649
Jonas Deng <zhiqin.deng@dfiretailgroup.com>
2025-07-16 17:29:16 +0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
   res.status(200).json({
-    jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21QYXJhbWV0ZXIiOiJxd2VyMTIzNCIsImV4cCI6MTc1MjU2OTg2MiwidXNlcklkIjoxNzM0NjkwfQ.qJGvFM2dxNZaGrHfid6pPaYwMM8selX_f1WpRAdAtVM"
+    jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21QYXJhbWV0ZXIiOiJxd2VyMTIzNCIsImV4cCI6MTc1MzI2Mjg5NSwidXNlcklkIjoxNzM0NjkwfQ.uGZRO2gdA2MXGL1rGslpA-MD8lX_s2zHw2q3qi-0ROw"
   });
 };
